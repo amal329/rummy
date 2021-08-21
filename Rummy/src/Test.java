fde82cc6958392e93269f227011d6729af430021
kudlamolka <57706178+kudlamolka@users.noreply.github.com>
2021-08-21 22:46:52 +0530

    Not Completed
    It works upto some level
    Has issues with the String Buffer,,,,Hence I have used Read.sc.nextInt or Read.sc.nextLine() twice
    Need to work on that and the Valuation process

--- Rummy/src/Test.java
+++ Rummy/src/Test.java
@@ -10,15 +10,54 @@ public class Test {
 		Player b = new Player("b");
 		Player c = new Player("c");
 		
-		Stock s= new Stock();
+		Stock stock = new Stock();
 		
 		CardDealer cd = new CardDealer();
 		cd.addPlayer(a);
 		cd.addPlayer(b);
 		cd.addPlayer(c);
 		
-		cd.distributeCards(d,s);
+		cd.distributeCards(d,stock);
 		
-		System.out.println(d.cards.size());
+		boolean gameStart = true;
+		
+		Card displayCard = stock.getTopCard();
+		
+		while(gameStart)
+		{
+			for(int i=0;i<3;i++)
+			{
+				Card returnedCard = cd.players.get(i).decidingOnCard(displayCard);
+				System.out.print("The Returned Card is ");
+				returnedCard.displayTheCard();
+				if((returnedCard.val == displayCard.val) && returnedCard.suit.equals(displayCard.suit))
+				{
+					stock.addCardToEnd(returnedCard);
+					displayCard = stock.getTopCard();
+					System.out.println("A Card is picked from Stock ");
+					displayCard = cd.players.get(i).decidingOnCard(displayCard);
+				}
+				else
+				{
+					displayCard = returnedCard;
+				}
+				
+				System.out.println("Do you want to Drop The Cards\n[Y]es   or [N]o");
+				String quit = Reader.sc.nextLine();
+				Reader.sc.nextLine();
+				if(quit.equalsIgnoreCase("Y"))
+				{
+					gameStart=false;
+					break;
+				}
+				System.out.println("\n***************\n");
+				
+					
+				
+				
+			}
+		}
+		
+		System.out.println("GAME OVER");
 	}
 }
